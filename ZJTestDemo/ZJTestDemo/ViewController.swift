01fd8200940428fd91795737b5f88348b2b6e96e
jiangxinfei <7682186+jiangxinfei@user.noreply.gitee.com>
2020-06-12 15:26:11 +0800

    第一次提交

---
+++ ZJTestDemo/ZJTestDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ZJTestDemo
+//
+//  Created by 智捷电商APP on 2020/6/12.
+//  Copyright © 2020 智捷电商APP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let name = "22222"
+        print(name)
+    }
+
+
+}
+
